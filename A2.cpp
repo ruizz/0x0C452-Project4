9f10b1f9ed6def03752b8e2af41bd09d0f7d4a40
ruizz <ruiz.akpan@gmail.com>
2013-04-08 17:43:21 -0700

    Initial completed code.

--- A2.cpp
+++ A2.cpp
@@ -1,205 +1,126 @@
-#include <stdio.h>
 #include "stddef.h"
 #include "glut.h"
+#include <iostream>
+#include <math.h>
 #include <vector>
 #include <fstream>
 #include <iostream>
 #include <sstream>
+#include <string>
+#include <iomanip>
+#include <fstream>
 
+#define PI 3.14159265
+
+static GLfloat move = 0.0;
 static GLfloat spin = 0.0;
 static GLfloat offsetX = 0;
 static GLfloat offsetY = 0;
-double PI = 3.14159265359;
 int polyToggle = 3;
 int startPosX = 0;
 int startPosY = 0;
 bool drag = false;
+double field[800][600];
 struct Car;
-struct Light;
-std::vector<Car> cars;
-std::vector<Light> lights;
+std::vector <Car> cars;
+double colorChange = 0.0;
+
 struct Point {
 
 	Point();
-	Point (int _x, int _y) : x(_x), y(_y) { }
-	int x;
-	int y;
+	Point (double _x, double _y) : x(_x), y(_y) { }
+	double x;
+	double y;
 };
-struct Car{
+
+struct Car {
 	Point location;
+	int k11, k12, k21, k22;
 	double rotation;
-	int k11;
-	int k12;
-	int k21;
-	int k22;
 
-	double movX;
-	double movY;
-	Car(Point _location, double _rotation, int _k11, int _k12, int _k21, int _k22)
-		:location(_location), rotation(_rotation), k11(_k11), k12(_k12), k21(_k21), k22(_k22){ movX=0; movY=0;}
+	Car();
+	Car(Point _location, double _rotation, int _k11, int _k12, int _k21, int _k22) 
+		: location(_location), rotation(_rotation), k11(_k11), k12(_k12), k21(_k21), k22(_k22) { }
 };
-struct Light{
-	Point loc;
-	int intensity;
-	Light(Point _loc, int _intensity)
-		:loc(_loc), intensity(_intensity) {}
-};
+
+void calculatePoint(int x, int y) {
+		for(int i=0; i<800; i++){
+			for(int j=0; j<600; j++){
+				double tempLight = sqrt((pow((x-i),2)+pow((y-j),2)));
+				if (field[i][j] < 100/tempLight){
+					field[i][j]=100/tempLight;
+				}
+			}
+		}
+	field[x][y]=100;
+}
+
+void drawPoint(int x, int y) {
+	glColor3f (1,1,1);
+	for(int i=0; i<800; i++){
+		for(int j=0; j<600; j++){
+			if (field[i][j] == 100) {
+			glBegin(GL_POINTS);
+			glVertex2f(i,j);
+			glEnd();
+			}
+		}		
+	}
+}
+
 void drawCar(Car c) {
-
-	Point location =    Point(0, 0);
-	Point front =       Point(0, -25);
-	Point backLeft =    Point(-15, 15);
-	Point backRight =   Point(15, 15);
-	Point leftSensor =  Point(-10, -25);
-	Point rightSensor = Point(10, -25);
 
 	glPushMatrix();
 	glTranslatef(c.location.x, c.location.y, 0);
-	glRotatef(c.rotation, 0.0, 0.0, 1.0);
+	glRotatef(-c.rotation + 90, 0.0, 0.0, 1.0);
 
 	// Draw car here.
 	glColor3f (1, 1, 1);
 	glBegin(GL_LINES);
 	glVertex2i (0, 0);
-	glVertex2i (-10, -25);
+	glVertex2i (-17, -17);
 	glVertex2i (0, 0);
-	glVertex2i (10, -25);
+	glVertex2i (17, -17);
 	glEnd();
 
-	glColor3f (1, 0, 0);
+	colorChange -= 0.0002;
+	if (colorChange < 0)
+		colorChange = 1.0;
+
+	glColor3f (c.k11 + colorChange, c.k12 + colorChange, c.k21 + colorChange);
 	glBegin(GL_POLYGON);
 	glVertex2i (0, -25);
-	glVertex2i (-15, 15);
-	glVertex2i (15, 15);
+	glVertex2i (-17, 15);
+	glVertex2i (17, 15);
 	glEnd();
 
 	glPopMatrix();
 }
-void drawPoint(int x, int y)
-{
-	//printf("called\n");
-	glBegin( GL_POINTS );
-	glColor3f( 0, 1, 0 );
-	glVertex2f(x,y);
-	glEnd();
 
-}
-void CalcMovVec(Car c)
-{
-	double mx=0;
-	double my=0;
-	double dist = sqrt(10*10 + 25*25);
-	//c.rotation=90;
-	double rot = -c.rotation + PI/2.0;
-	double angle = atan(10.0/25.0) + rot/180.0*PI;
-	double angle2 = -atan(10.0/25.0) + rot/180.0*PI;
-
-	
-	Point leftSloc = Point(c.location.x-dist*(sin(angle) ), c.location.y-dist*cos(angle));
-	Point rightSloc = Point(c.location.x-dist*(sin(angle2) ), c.location.y-dist*cos(angle2));
-
-	/*
-	printf("%f\n", angle*180/PI);
-	printf("%f\n", angle2*180/PI);
-	printf("%f %f\n", sin(angle), cos(angle));
-	printf("Car pos %d %d\n", c.location.x, c.location.y);
-	printf("Left %d %d, Right \n",leftSloc.x, leftSloc.y);
-	printf("Left %d %d, Right \n",leftSloc.x, leftSloc.y);
-	*/
-
-	drawPoint(leftSloc.x, leftSloc.y);
-	drawPoint(rightSloc.x, rightSloc.y);
-
-	
-	
-	for(int i=0; i<lights.size(); i++)
-	{
-		double distl = sqrt(pow((double)(lights[i].loc.x - leftSloc.x), 2.0) + pow((double)(lights[i].loc.y - leftSloc.y), 2.0));
-		double distr = sqrt(pow((double)(lights[i].loc.x - rightSloc.x), 2.0) + pow((double)(lights[i].loc.y - rightSloc.y), 2.0));
-		double leftAngle = asin((double)(-lights[i].loc.y + leftSloc.y)/distl);	
-		double rightAngle = asin((double)(-lights[i].loc.y + rightSloc.y)/distr);
-		
-		if(lights[i].loc.x < leftSloc.x)
-			leftAngle = PI - leftAngle;
-		if(lights[i].loc.x < rightSloc.x)
-			rightAngle = PI - rightAngle;
-
-		printf("%f\n", leftAngle*180/PI);
-		printf("%f\n", rightAngle*180/PI);
-		mx+=((lights[i].intensity/distl)*cos(leftAngle)+(lights[i].intensity/distr)*cos(rightAngle))/2.0;
-		my-=((lights[i].intensity/distl)*sin(leftAngle)+(lights[i].intensity/distr)*sin(rightAngle))/2.0;
-	}
-	printf("Pointing %f %f\n", mx, my);
-	
-	glColor3f (0, 0, 1);
-	glLineWidth(4);
-	glBegin(GL_LINES);
-	glVertex2i (c.location.x, c.location.y);
-	glVertex2i (c.location.x + mx*10, c.location.y + my*10);
-	glEnd();
-}
-void drawLight(Light l)
-{
-	//printf("called\n");
-	glBegin( GL_POINTS );
-	int light_size = 2;
-	double unit = (double)(l.intensity/(light_size*2+1));
-	/*
-	for(int i=-light_size; i<=light_size; i++)
-	{
-		for(int j=-light_size; j<=light_size; j++)
-		{
-			double dist = sqrt(i*i + j*j);
-			printf("%d %d %f\n", i,j,dist);
-			//glColor3f( (double)l.intensity/(dist*100), (double)l.intensity/(dist*100), (double)l.intensity/(dist*100) );
-			glColor3f( (double)(l.intensity - dist*unit)/100.0, (double)(l.intensity - dist*unit)/100.0, (double)(l.intensity - dist*unit)/100.0 );
-			glVertex2f(l.loc.x+i,l.loc.y+j);
-		}
-	}
-	*/
-	glColor3f( (double)l.intensity/100.0, (double)l.intensity/100.0, (double)l.intensity/100.0 );
-	glVertex2f(l.loc.x,l.loc.y);
-	glEnd();
-	//exit(1);
-}
 
 void init(void) {
 
 	std::ifstream ifs("input.txt");
-	std::string sNumOfCars;
+	std::string sNumOfCars = "";
+	std::string sCarData = "";
 
-	getline(ifs, sNumOfCars);
-	int numCars = atoi(sNumOfCars.c_str());
+	while (ifs) {
+		getline(ifs, sNumOfCars);
+		int numOfCars = atoi(sNumOfCars.c_str());
+		for (int i = 0; i < numOfCars; i++) {
+			std::string carX, carY, carR, carK11, carK12, carK21, carK22;
 
-	for (int i=0; i<numCars; i++) {
-		std::string sx, sy, srot, sk11, sk12, sk21, sk22;
-		ifs>>sx;
-		ifs>>sy;
-		ifs>>srot;
-		ifs>>sk11;
-		ifs>>sk12;
-		ifs>>sk21;
-		ifs>>sk22;
-
-		int x, y, k11, k12, k21, k22;
-		double rot;
-		
-		x = atoi(sx.c_str());
-		y = atoi(sy.c_str());
-		rot = atof(srot.c_str());
-		k11 = atoi(sk11.c_str());
-		k12 = atoi(sk12.c_str());
-		k21 = atoi(sk21.c_str());
-		k22 = atoi(sk22.c_str());
-
-		cars.push_back(Car(Point(x,y), rot, k11, k12, k21, k22));
-
+			getline(ifs, sCarData);
+			std::stringstream carData(sCarData);
+			carData >> carX >> carY >> carR >> carK11 >> carK12 >> carK21 >> carK22;
+			std::cout << carX << carY << carR << carK11 << carK12 << carK21 << carK22;
+			cars.push_back(Car(Point(atof(carX.c_str()), atof(carY.c_str())), atoi(carR.c_str()), atoi(carK11.c_str()), atoi(carK12.c_str()), atoi(carK21.c_str()), atoi(carK22.c_str())));
+		}
 	}
 
-	//cars.push_back(Car(Point(100,200), 90, 1, 0, 0, 1));
 	glClearColor(0.0, 0.0, 0.0, 0.0);
 	glShadeModel(GL_FLAT);
-	glPointSize(6.0);
+	glPointSize(2.0);
 
 	// Set projection parameters.
 	glMatrixMode (GL_PROJECTION);
@@ -211,36 +132,55 @@ void polygon(void) {
 
 	glClear(GL_COLOR_BUFFER_BIT);
 
-	// This is where you draw ANY car. Orgin in top-left corner.
-	for(int i=0; i<lights.size(); i++)
-	{
-		drawLight(lights[i]);
-	}
-	
-	// drawCar(x, y, degree, red, green, blue);
-	for(int i=0; i<cars.size(); i++)
-	{
+	drawPoint(startPosX, startPosY);
+
+	for (int i = 0; i < cars.size(); i++ ) {
 		drawCar(cars[i]);
-		CalcMovVec(cars[i]);
 	}
 	
+
 	glutSwapBuffers();
 }
 
+// Calculations all done here for car.
+void moveCar(void) {
 
-void spinDisplayL(void) {
+	for (int i = 0; i < cars.size(); i++ ) {
+		double w1, w2;
+		// -- drewsatmid suggestion -- 
+		//read in sensor values S1 and S2
+		//matrix multiplication [S1 S2][k11 k12 k21 k22]=[w1 w2]
 
-	spin = spin + 0.5;
-	if (spin > 360.0)
-		spin = spin-360.0;
-	glutPostRedisplay();
-}
+		// This is confirmed accurate. Exact positions of the car's sensors.
+		Point sensor1 = Point(cars[i].location.x + 25*cos(((45-cars[i].rotation)*PI)/180), cars[i].location.y + 25*sin(((45-cars[i].rotation)*PI)/180));
+		Point sensor2 = Point(cars[i].location.x + 25*cos(((-45-cars[i].rotation)*PI)/180), cars[i].location.y + 25*sin(((-45-cars[i].rotation)*PI)/180));
+		
+		// Values of the car's sensors. Confirmed accurate.
+		double sensorValue1 = field[(int)sensor1.x][(int)sensor1.y];
+		double sensorValue2 = field[(int)sensor2.x][(int)sensor2.y];
 
-void spinDisplayR(void) {
+		// Cannot confirm if accurate.
+		w1 = sensorValue1*cars[i].k11 + sensorValue2*cars[i].k12;
+		w2 = sensorValue1*cars[i].k21 + sensorValue2*cars[i].k22;
 
-	spin = spin - 0.5;
-	if (spin < -360.0)
-		spin = spin+360.0;
+		cars[i].rotation += (w2-w1);
+
+		// This is not accurate.
+		double moveX = cos(((360-cars[i].rotation)*PI)/180)*(w1+w2)/25;
+		double moveY = sin(((360-cars[i].rotation)*PI)/180)*(w1+w2)/25;
+
+		cars[i].location.x +=moveX;
+		cars[i].location.y +=moveY;
+
+		if (cars[i].location.x > 775)
+			cars[i].location.x = 25;
+		if (cars[i].location.x < 25)
+			cars[i].location.x = 775;
+		if (cars[i].location.y > 575)
+			cars[i].location.y = 25;
+		if (cars[i].location.y < 25)
+			cars[i].location.y = 575;
+	}
 	glutPostRedisplay();
 }
 
@@ -258,39 +198,23 @@ void mouse (int button, int state, int x, int y) {
 
 	switch(button) {
 	case GLUT_LEFT_BUTTON:
+		
+
 		if (state == GLUT_DOWN) {
 			startPosX = x;
 			startPosY = y;
-			lights.push_back(Light(Point(x,y), 100));
-			glutIdleFunc(spinDisplayL);
+			calculatePoint(startPosX, startPosY);
+			glutIdleFunc(moveCar);
 			
 		}
 		if (state == GLUT_UP) {
-			startPosX = 0;
-			startPosY = 0;
-			//glutIdleFunc(NULL);
-		}
-		break;
-	case GLUT_RIGHT_BUTTON:
-		if (state == GLUT_DOWN) {
-			startPosX = x;
-			startPosY = y;
-			glutIdleFunc(spinDisplayR);
-		}
-		if (state == GLUT_UP) {
-			startPosX = 0;
-			startPosY = 0;
-			glutIdleFunc(NULL);
+
 		}
 		break;
 	default:
 		break;
 	}
 }
-
-
-// Request double buffer polygon mode.
-// Register mouse input callback functions.
 
 int main (int argc, char ** argv) {
 
